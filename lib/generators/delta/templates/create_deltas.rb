90dcad7e99a808b12e8d55d517eec99b2a0663c1
Theodore Konukhov <komolov.f@gmail.com>
2015-09-08 17:00:29 +0300

    remove foreign_key from migration. TODO: make it configurable

--- lib/generators/delta/templates/create_deltas.rb
+++ lib/generators/delta/templates/create_deltas.rb
@@ -2,7 +2,7 @@ class CreateDeltas < ActiveRecord::Migration
   def change
     create_table :deltas, force: :cascade do |t|
       # TODO: custom users key name
-      t.references :user, index: true, foreign_key: true
+      t.references :user, index: true
       t.references :model, index: true, polymorphic: true
       t.json       :object, null: false # TODO -> text if JSON is not supported
 
