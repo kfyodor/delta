93e0cbe27abd9e7e24f3dd2c4ad8b5032b176e4f
Theodore Konukhov <komolov.f@gmail.com>
2015-10-06 16:23:50 +0300

    a config option for not using after_commit callbacks

--- lib/delta/config.rb
+++ lib/delta/config.rb
@@ -6,12 +6,14 @@ module Delta
     attr_reader   :adapters
 
     attr_accessor :controller_user_method,
-                  :user_model
+                  :user_model,
+                  :dont_use_after_commit_callbacks
 
     def initialize
-      @adapters               = Adapters.new(["active_record"])
-      @user_model             = :user
-      @controller_user_method = :current_user
+      @adapters                        = Adapters.new(["active_record"])
+      @user_model                      = :user
+      @dont_use_after_commit_callbacks = false
+      @controller_user_method          = :current_user
     end
 
     def adapters=(adapter_names)
